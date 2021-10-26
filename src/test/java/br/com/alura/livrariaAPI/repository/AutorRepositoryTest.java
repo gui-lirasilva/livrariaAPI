5bab54079b1b0e4df76251d02bd889763473c77b
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-10-26 19:43:44 -0300

    Finalizando testes da AutorRepository

--- src/test/java/br/com/alura/livrariaAPI/repository/AutorRepositoryTest.java
+++ src/test/java/br/com/alura/livrariaAPI/repository/AutorRepositoryTest.java
@@ -28,60 +28,111 @@ class AutorRepositoryTest {
 	private TestEntityManager testEntityManager;
 
 	@Test
-	  void deveriaCadastrarUmAutor() {
-	    Autor autor = new Autor(
-	    		"Arthur Conan Doyle", 
-	    		LocalDate.parse("1859-05-22"), 
-	    		"arthur@gmail.com",  
-	    		"Autor das historias de Sherlock Holmes");
-	    testEntityManager.persist(autor);
-	    Assertions.assertThat(autorRepository.findAll().contains(autor)).isTrue();
-	  }
+	void deveriaCadastrarUmAutor() {
+		Autor autor = new Autor(
+				"Arthur Conan Doyle", 
+				LocalDate.parse("1859-05-22"), 
+				"arthur@gmail.com",
+				"Autor das historias de Sherlock Holmes");
+		testEntityManager.persist(autor);
+		Assertions.assertThat(autorRepository.findAll().contains(autor)).isTrue();
+	}
+
+	@Test
+	void deveriaListarAutores() {
+		Autor autor1 = new Autor(
+				"Nome do autor 1", 
+				LocalDate.parse("1869-05-22"), 
+				"autor1@gmail.com",
+				"Autor dos livros de ficcao");
+		testEntityManager.persist(autor1);
+
+		Autor autor2 = new Autor(
+				"Nome do autor 2", 
+				LocalDate.parse("1879-06-23"), 
+				"autor2@gmail.com",
+				"Autor dos livros de fantasia");
+		testEntityManager.persist(autor2);
+
+		Autor autor3 = new Autor(
+				"Nome do autor 3", 
+				LocalDate.parse("1889-07-24"), 
+				"autor3@gmail.com",
+				"Autor dos livros de fabulas");
+		testEntityManager.persist(autor3);
+
+		List<Autor> autores = autorRepository.findAll();
+		Assertions
+			.assertThat(autores)
+			.hasSize(3)
+			.extracting(Autor::getNome, Autor::getDataNascimento, Autor::getEmail, Autor::getMiniCurriculo)
+			.containsExactlyInAnyOrder(
+			Assertions.tuple(
+					"Nome do autor 1", 
+					LocalDate.parse("1869-05-22"), 
+					"autor1@gmail.com",
+					"Autor dos livros de ficcao"),
+			Assertions.tuple(
+					"Nome do autor 2", 
+					LocalDate.parse("1879-06-23"), 
+					"autor2@gmail.com",
+					"Autor dos livros de fantasia"),
+			Assertions.tuple(
+					"Nome do autor 3", 
+					LocalDate.parse("1889-07-24"), 
+					"autor3@gmail.com",
+					"Autor dos livros de fabulas"));
+
+	}
 	
 	@Test
-	  void deveriaListarAutores() {
-	    Autor autor1 = new Autor(
-	    		"Nome do autor 1", 
-	    		LocalDate.parse("1869-05-22"), 
-	    		"autor1@gmail.com",  
-	    		"Autor dos livros de ficcao");
-	    testEntityManager.persist(autor1);
-	    
-	    Autor autor2 = new Autor(
-	    		"Nome do autor 2", 
-	    		LocalDate.parse("1879-06-23"), 
-	    		"autor2@gmail.com",  
-	    		"Autor dos livros de fantasia");
-	    testEntityManager.persist(autor2);
-	    
-	    Autor autor3 = new Autor(
-	    		"Nome do autor 3", 
-	    		LocalDate.parse("1889-07-24"), 
-	    		"autor3@gmail.com",  
-	    		"Autor dos livros de fabulas");
-	    testEntityManager.persist(autor3);
+	void deveriaAtualizarUmAutor() {
+		Autor autor = new Autor(
+				"Nome de um autor qualquer", 
+				LocalDate.parse("1969-11-15"), 
+				"autorQualquer@gmail.com",
+				"Autor de varios livros");
+		testEntityManager.persist(autor);
 
-	    List<Autor> autores = autorRepository.findAll();
-	    Assertions.assertThat(autores).hasSize(3).
-	      extracting(Autor::getNome,  Autor::getDataNascimento, Autor::getEmail, Autor::getMiniCurriculo)
-	      .containsExactlyInAnyOrder(
-	        Assertions.tuple(
-	        		"Nome do autor 1", 
-		    		LocalDate.parse("1869-05-22"), 
-		    		"autor1@gmail.com",  
-		    		"Autor dos livros de ficcao"),
-	        Assertions.tuple(
-	        		"Nome do autor 2", 
-		    		LocalDate.parse("1879-06-23"), 
-		    		"autor2@gmail.com",  
-		    		"Autor dos livros de fantasia"),
-	        Assertions.tuple(
-	        		"Nome do autor 3", 
-		    		LocalDate.parse("1889-07-24"), 
-		    		"autor3@gmail.com",  
-		    		"Autor dos livros de fabulas")
-	      );
-	    
-	    
+		autor.atualizarInformacoes(
+				"Bob Kane", 
+				LocalDate.parse("1915-10-24"), 
+				"bobMorcego@gmail.com",
+				"Co-autor e criador do Batman nos quadrinhos");
+		testEntityManager.merge(autor);
+
+		List<Autor> autores = autorRepository.findAll();
+		Assertions.assertThat(autores).hasSize(1)
+				.extracting(Autor::getNome, Autor::getDataNascimento, Autor::getEmail, Autor::getMiniCurriculo)
+				.contains(Assertions.tuple(
+						"Bob Kane", 
+						LocalDate.parse("1915-10-24"), 
+						"bobMorcego@gmail.com",
+						"Co-autor e criador do Batman nos quadrinhos"));
+	}
+	
+	@Test
+	  void deveriaRemoverUmAutor() {
+	    Autor autor = new Autor(
+	    		"Nome de um autor qualquer", 
+				LocalDate.parse("1969-11-15"), 
+				"autorQualquer@gmail.com",
+				"Autor de varios livros");
+	    testEntityManager.persistAndGetId(autor);
+	    testEntityManager.remove(autor);
+	    Assertions.assertThat(autorRepository.findAll()).isEmpty();
 	  }
+	
+	 @Test
+	  void deveriaEncontrarUmAutorPorId() {
+	    Autor autor = new Autor(
+	    		"Isaac Asimov", 
+	    		LocalDate.parse("1920-01-02"), 
+	    		"asimov@gmail.com", 
+	    		"Um dos mestres da ficcao cientifica");
+	    Long autorId = testEntityManager.persistAndGetId(autor, Long.class);
+
+	    Assertions.assertThat(autorRepository.getById(autorId)).isNotNull();
+	  }
+
 }
