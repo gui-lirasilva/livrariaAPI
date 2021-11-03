2e92e34afe22978847643abbb5c6a8eacec7e2b6
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-11-02 22:21:16 -0300

    Adicionando testes da RelatorioLivrosAutorController

---
+++ src/test/java/br/com/alura/livrariaAPI/controller/RelatorioLivrosAutorControllerTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.livrariaAPI.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class RelatorioLivrosAutorControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void deveriaRetornarRelatorioComStatus200() throws Exception {
+		mockMvc.perform(get("/relatorios").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+}
