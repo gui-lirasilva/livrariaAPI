379b82bc7e64353781114feee23ecef335b2b94e
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-11-19 23:21:50 -0300

    Modificando AutorDto

--- src/main/java/br/com/alura/livrariaAPI/dto/AutorDto.java
+++ src/main/java/br/com/alura/livrariaAPI/dto/AutorDto.java
@@ -4,19 +4,29 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AutorDto {
-	
+
 	private Long id;
 	private String nome;
 	private String email;
 	private String miniCurriculo;
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataNascimento;
-	
+
+	public AutorDto(String nome, String email, String miniCurriculo, LocalDate dataDeNascimento) {
+		this.nome = nome;
+		this.email = email;
+		this.miniCurriculo = miniCurriculo;
+		this.dataNascimento = dataDeNascimento;
+	}
 }
